178a5aa0afc080e49804589ed48834fd3410eb19
Deepakrao64 <deepakjadhav9224@gmail.com>
2021-06-05 13:17:55 +0530

    NumberSwapping.java

--- Java/src/test/java/NumberSwapping.java
+++ Java/src/test/java/NumberSwapping.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class NumberSwapping {
+
+public class NumberSwapping {
+
+    public void swappingOfNumbers(int x, int y) {
+        x = x+y;
+        y = x-y;
+        x = x-y;
+        System.out.println(x);
+        System.out.println(y);
+    }
+
+    public void swappingOfNumbersUsingMultipleOperator(int x,int y) {
+        x = x * y;
+        y = x / y;
+        x = x / y;
+        System.out.println(x);
+        System.out.println(y);
+    }
+
+    public void swappingNumberUsingXOR(int x, int y){
+        x = x ^ y;
+        y = x ^ y;
+        x = x ^ y;
+        System.out.println(x);
+        System.out.println(y);
+    }
+
+    public void swappingNumberUsingThirdVariable(int x, int y) {
+        int z = x;
+        x = y;
+        y = z;
+        System.out.println(x);
+        System.out.println(y);
+    }
 }
